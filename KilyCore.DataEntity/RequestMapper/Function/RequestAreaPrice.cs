e7c3320b631b2b97535b881b75fbb53d7413b8f0
Emily <847432003@qq.com>
2018-04-20 09:52:29 +0800

    Revert "重置版本"
    This reverts commit 24505d6813ec2cd79b9be2d17a6feeb0776a18fb.

--- KilyCore.DataEntity/RequestMapper/Function/RequestAreaPrice.cs
+++ KilyCore.DataEntity/RequestMapper/Function/RequestAreaPrice.cs
@@ -8,14 +8,23 @@ namespace KilyCore.DataEntity.RequestMapper.Function
     public class RequestAreaPrice
     {
         public Guid Id { get; set; }
+        [Mapper(Alter = Updates.Require)]
         public string ProjectName { get; set; }
+        [Mapper(Alter = Updates.Require)]
         public decimal? ProvincePrice { get; set; }
+        [Mapper(Alter = Updates.Require)]
         public decimal? CityPrice { get; set; }
+        [Mapper(Alter = Updates.Require)]
         public decimal? AreaPrice { get; set; }
+        [Mapper(Alter = Updates.Require)]
         public decimal? TownPrice { get; set; }
+        [Mapper(Alter = Updates.Require)]
         public Guid? ProvinceId { get; set; }
+        [Mapper(Alter = Updates.Require)]
         public Guid? CityId { get; set; }
+        [Mapper(Alter = Updates.Require)]
         public Guid? AreaId { get; set; }
-        public Guid? TwonId { get; set; }
+        [Mapper(Alter = Updates.Require)]
+        public Guid? TownId { get; set; }
     }
 }
--- KilyCore.Repositories/BaseRepository/IRepository.cs
+++ KilyCore.Repositories/BaseRepository/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -9,11 +10,12 @@ namespace KilyCore.Repositories.BaseRepository
     public interface IRepository
     {
         bool Insert<TEntity>(TEntity entity) where TEntity : class, new();
-        bool Update<TEntity, DEntity>(TEntity entity, DEntity dto) where TEntity : class, new() where DEntity : class, new();
-        bool UpdateField<TEntity>(TEntity entity, string field, List<string> fields = null) where TEntity : class, new();
+        bool Update<TEntity, DEntity>(TEntity entity, DEntity dto, PropertyDescriptorCollection collection = null) where TEntity : class, new() where DEntity : class, new();
+        bool UpdateField<TEntity>(TEntity entity, string field, IList<String> fields = null) where TEntity : class, new();
         bool Delete<TEntity>(Expression<Func<TEntity, bool>> exp) where TEntity : class, new();
         bool Remove<TEntity>(Expression<Func<TEntity, bool>> exp) where TEntity : class, new();
         IQueryable<TEntity> ExcuteSQL<TEntity> (string SQL) where TEntity : class, new();
         int ExcuteSQL(string SQL);
+        PropertyDescriptorCollection PropertyCollection<DEntity, TAttribute>(DEntity entity, Object value) where DEntity : class, new() where TAttribute : Attribute;
     }
 }
