610d29fcb212a92326bd8d213fcee57f4e886dba
Emily <847432003@qq.com>
2018-06-08 14:41:09 +0800

    添加设备模型

---
+++ KilyCore.DataEntity/ResponseMapper/Enterprise/ResponseEnterpriseDevice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 类 名 称 ：ResponseEnterpriseDevice
+* 类 描 述 ：
+* 命名空间 ：KilyCore.DataEntity.ResponseMapper.Enterprise
+* 机器名称 ：DESKTOP-QPIVQ28 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：刘泽华
+* 创建时间 ：2018/6/8 14:39:24
+*******************************************************************
+* Copyright @ 刘泽华 2018. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+namespace KilyCore.DataEntity.ResponseMapper.Enterprise
+{
+    public class ResponseEnterpriseDevice
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public string DeviceName { get; set; }
+        public string ModelName { get; set; }
+        public string SupplierName { get; set; }
+        public DateTime? ProductTime { get; set; }
+        public string Life { get; set; }
+        public string Manager { get; set; }
+    }
+    public class ResponseEnterpriseDeviceClean
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public Guid DeviceId { get; set; }
+        public string Manager { get; set; }
+        public DateTime? CleanTime { get; set; }
+        public string Ways { get; set; }
+    }
+    public class ResponseEnterpriseDeviceFix
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public Guid DeviceId { get; set; }
+        public DateTime? FixTime { get; set; }
+        public string Reason { get; set; }
+        public string Manager { get; set; }
+    }
+}
---
+++ KilyCore.EntityFrameWork/EntityMapping/Enterprise/EnterpriseDeviceMap.cs
@@ -0,0 +1,51 @@
+﻿using KilyCore.EntityFrameWork.Model.Enterprise;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 类 名 称 ：EnterpriseDeviceMap
+* 类 描 述 ：
+* 命名空间 ：KilyCore.EntityFrameWork.EntityMapping.Enterprise
+* 机器名称 ：DESKTOP-QPIVQ28 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：刘泽华
+* 创建时间 ：2018/6/8 14:24:46
+*******************************************************************
+* Copyright @ 刘泽华 2018. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+namespace KilyCore.EntityFrameWork.EntityMapping.Enterprise
+{
+    public class EnterpriseDeviceMap : IEntityTypeConfiguration<EnterpriseDevice>
+    {
+        public void Configure(EntityTypeBuilder<EnterpriseDevice> builder)
+        {
+            builder.ToTable(typeof(EnterpriseDevice).Name);
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.ProductTime).HasColumnType(typeof(DateTime).Name);
+        }
+    }
+    public class EnterpriseDeviceCleanMap : IEntityTypeConfiguration<EnterpriseDeviceClean>
+    {
+        public void Configure(EntityTypeBuilder<EnterpriseDeviceClean> builder)
+        {
+            builder.ToTable(typeof(EnterpriseDeviceClean).Name);
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.CleanTime).HasColumnType(typeof(DateTime).Name);
+        }
+    }
+    public class EnterpriseDeviceFixMap : IEntityTypeConfiguration<EnterpriseDeviceFix>
+    {
+        public void Configure(EntityTypeBuilder<EnterpriseDeviceFix> builder)
+        {
+            builder.ToTable(typeof(EnterpriseDeviceFix).Name);
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.FixTime).HasColumnType(typeof(DateTime).Name);
+        }
+    }
+}
