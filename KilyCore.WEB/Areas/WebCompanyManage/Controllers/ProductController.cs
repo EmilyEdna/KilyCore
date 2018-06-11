0a4707e5257b8a1d958588a78b4f3a13302d6e3a
刘旭林 <liuxulinasp@126.com>
2018-06-11 17:31:36 +0800

    生产管理页面

---
+++ KilyCore.WEB/Areas/WebCompanyManage/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KilyCore.WEB.Areas.WebCompanyManage.Controllers
+{
+    /// <summary>
+    /// 生产管理
+    /// </summary>
+    [Area("WebCompanyManage")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region 生产管理
+        /// <summary>
+        /// 设备管理
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Device()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 编辑设备
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult EditDevice()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
