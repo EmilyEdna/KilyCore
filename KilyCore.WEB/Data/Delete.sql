266e2d14d541f29a38215d7a4c6be9f2a266ac8b
Emily <847432003@qq.com>
2018-06-12 17:24:17 +0800

    修改BUG

--- KilyCore.WEB/Data/Delete.sql
+++ KilyCore.WEB/Data/Delete.sql
@@ -1,22 +1,22 @@
 ﻿USE [Kily];
 GO
 --删除所有约束
-DECLARE c1 CURSOR FOR
+DECLARE Temp CURSOR FOR
 SELECT 'alter table [' + OBJECT_NAME(parent_obj) + '] drop constraint [' + name + ']; '
 FROM sysobjects
 WHERE xtype = 'F';
-OPEN c1;
-DECLARE @c1 VARCHAR(8000);
-FETCH NEXT FROM c1
-INTO @c1;
+OPEN Temp;
+DECLARE @Temp VARCHAR(8000);
+FETCH NEXT FROM Temp
+INTO @Temp;
 WHILE (@@fetch_status = 0)
 BEGIN
-    EXEC (@c1);
-    FETCH NEXT FROM c1
-    INTO @c1;
+    EXEC (@Temp);
+    FETCH NEXT FROM Temp
+    INTO @Temp;
 END;
-CLOSE c1;
-DEALLOCATE c1;
+CLOSE Temp;
+DEALLOCATE Temp;
 --删除数据库所有表
 DECLARE @tname VARCHAR(8000);
 SET @tname = '';
