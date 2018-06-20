1f0c831d17360ac94722161abdd725204d83bd66
刘旭林 <liuxulinasp@126.com>
2018-06-20 11:10:41 +0800

    原材料质检

---
+++ KilyCore.WEB/Areas/WebCompanyManage/Controllers/QualityController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KilyCore.WEB.Areas.WebCompanyManage.Controllers
+{
+    [Area("WebCompanyManage")]
+    public class QualityController : Controller
+    {
+        /// <summary>
+        /// 原料质检
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult RawCheck()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 编辑原料质检
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult RawCheckEdit()
+        {
+            return View();
+        }
+        
+    }
+}
