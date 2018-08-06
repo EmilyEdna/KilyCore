3743a68638f5d3be471775bd7b0a09a958fcf414
Emily <847432003@qq.com>
2018-08-06 11:44:25 +0800

    添加相关页面和ctrl

---
+++ KilyCore.WEB/Areas/WebMerchantManage/Controllers/SourceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KilyCore.WEB.Areas.WebMerchantManage.Controllers
+{
+    [Area("WebMerchantManage")]
+    public class SourceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
