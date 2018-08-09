2b981f61e051f1ec3d8b68ebecf94b4f03358869
Emily <847432003@qq.com>
2018-08-09 11:47:32 +0800

    新增物品仓库表

---
+++ KilyCore.EntityFrameWork/Model/Repast/RepastArticleInStock.cs
@@ -0,0 +1,68 @@
+﻿using KilyCore.EntityFrameWork.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 类 名 称 ：RepastArticleInStock
+* 类 描 述 ：
+* 命名空间 ：KilyCore.EntityFrameWork.Model.Repast
+* 机器名称 ：EMILY 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：$刘泽华$
+* 创建时间 ：2018/8/9 11:10:18
+*******************************************************************
+* Copyright @ $刘泽华$ 2018. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+namespace KilyCore.EntityFrameWork.Model.Repast
+{
+    /// <summary>
+    /// 物品入库表
+    /// </summary>
+    public class RepastArticleInStock: RepastBase
+    {
+        /// <summary>
+        /// 批次号
+        /// </summary>
+        public virtual string BatchNo { get; set; }
+        /// <summary>
+        /// 物品名称
+        /// </summary>
+        public virtual string ArticleName { get; set; }
+        /// <summary>
+        /// 入库数量
+        /// </summary>
+        public virtual int InStockNum { get; set; }
+        /// <summary>
+        /// 单价
+        /// </summary>
+        public virtual decimal? PrePrice { get; set; }
+        /// <summary>
+        /// 总价
+        /// </summary>
+        public virtual decimal? ToPrice { get; set; }
+        /// <summary>
+        /// 供应商
+        /// </summary>
+        public virtual string Supplier { get; set; }
+        /// <summary>
+        /// 生产地址
+        /// </summary>
+        public virtual string Address { get; set; }
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public virtual string Phone { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public virtual string Remark { get; set; }
+        /// <summary>
+        /// 采购负责人
+        /// </summary>
+        public virtual string BuyUser { get; set; }
+    }
+}
---
+++ KilyCore.EntityFrameWork/Model/Repast/RepastArticleOutStock.cs
@@ -0,0 +1,52 @@
+﻿using KilyCore.EntityFrameWork.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 类 名 称 ：RepastArticleOutStock
+* 类 描 述 ：
+* 命名空间 ：KilyCore.EntityFrameWork.Model.Repast
+* 机器名称 ：EMILY 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：$刘泽华$
+* 创建时间 ：2018/8/9 11:10:34
+*******************************************************************
+* Copyright @ $刘泽华$ 2018. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+namespace KilyCore.EntityFrameWork.Model.Repast
+{
+    /// <summary>
+    /// 物品出库表
+    /// </summary>
+    public class RepastArticleOutStock: RepastBase
+    {
+        /// <summary>
+        /// 入库的Id
+        /// </summary>
+        public virtual Guid InStockId { get; set; }
+        /// <summary>
+        /// 批次号
+        /// </summary>
+        public virtual string BatchNo { get; set; }
+        /// <summary>
+        /// 食材名称
+        /// </summary>
+        public virtual string ArticleName { get; set; }
+        /// <summary>
+        /// 出库数量
+        /// </summary>
+        public virtual int OutStockNum { get; set; }
+        /// <summary>
+        /// 出库时间
+        /// </summary>
+        public virtual DateTime? OutStockTime { get; set; }
+        /// <summary>
+        /// 出库负责人
+        /// </summary>
+        public virtual string OutUser { get; set; }
+    }
+}
