5b03f793f5fafaf969ccdf0dddec867acd71823c
Emily <847432003@qq.com>
2018-09-07 09:55:14 +0800

    添加过滤结果返回

--- KilyCore.Extension/FilterGroup/ResultFilter.cs
+++ KilyCore.Extension/FilterGroup/ResultFilter.cs
@@ -1,6 +1,5 @@
 ﻿using KilyCore.Extension.ResultExtension;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -32,42 +31,29 @@ namespace KilyCore.Extension.FilterGroup
             if (context.Result is ObjectResultEx)
             {
                 var res = context.Result as ObjectResultEx;
-                context.Result = new ObjectResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = Data(context, res) ?? res.Value });
+                context.Result = new ObjectResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = ResultContext.FilterResult(context.HttpContext, res) ?? res.Value });
             }
             if (context.Result is EmptyResultEx)
             {
                 var res = context.Result as EmptyResultEx;
 
-                context.Result = new EmptyResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = res.Value });
+                context.Result = new EmptyResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = ResultContext.FilterResult(context.HttpContext, res) ?? res.Value });
             }
             if (context.Result is ContentResultEx)
             {
                 var res = context.Result as ContentResultEx;
-                context.Result = new ContentResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = res.Value });
+                context.Result = new ContentResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = ResultContext.FilterResult(context.HttpContext, res) ?? res.Value });
             }
             if (context.Result is StatusCodeResultEx)
             {
                 var res = context.Result as StatusCodeResultEx;
-                context.Result = new StatusCodeResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = res.Value });
+                context.Result = new StatusCodeResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = ResultContext.FilterResult(context.HttpContext, res) ?? res.Value });
             }
             if (context.Result is JsonResultEx)
             {
                 var res = context.Result as JsonResultEx;
-                context.Result = new JsonResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = res.Value });
+                context.Result = new JsonResultEx(new { HttpCode = res.Code, msg = res.Messege, flag = res.Flag, data = ResultContext.FilterResult(context.HttpContext, res) ?? res.Value });
             }
-        }
-        public Object Data(ResultExecutingContext context, ObjectResultEx res)
-        {
-            if (context.HttpContext.Request.Headers["PC"].Equals("PC"))
-            {
-                string data = res.Value.ToString();
-                if (data.Contains("CookAdmin") || data.Contains("SysAdmin") || data.Contains("ComAdmin") || data.Contains("RepAdmin") || data.Contains("GovtInfo"))
-                    return JsonConvert.DeserializeObject(JsonConvert.SerializeObject(res.Value).Replace("\"PassWord\":\"admin\",", ""));
-                else
-                    return null;
-            }
-            else
-                return null;
         }
     }
 }
