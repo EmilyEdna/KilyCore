0c87eeef8d44347d0b541cdebcfbc61b2e5baed9
刘旭林 <liuxulinasp@126.com>
2019-10-22 10:40:23 +0800

    产品信息结果增加

---
+++ KilyCore.WEB/Areas/WebMerchantManage/Controllers/APPController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KilyCore.WEB.Areas.WebMerchantManage.Controllers
+{
+    [Area("WebMerchantManage")]
+    public class APPController : Controller
+    {
+        public IActionResult ReportTable()
+        {
+            return View();
+        }
+       
+    }
+}
